351d4a400640371360535f4e45d3e454b59c20d2
pavank9506 <125499392+pavank9506@users.noreply.github.com>
2026-02-04 13:52:45 +0530

    Define security group and ingress rules variables
    Added security group variable and ingress rules for ports 22, 80, and 8080.

--- Terraform-module-aws-vpc/VPC/variables.tf
+++ Terraform-module-aws-vpc/VPC/variables.tf
@@ -62,3 +62,32 @@ variable "private_route_table_tags" {
 variable "database_route_table_tags" {
     default = {}
 }
+
+variable "sg" {
+}
+
+variable "ingress_rules" {
+    default = [
+        {
+            from_port = 22
+            to_port = 22
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "opening 22"
+        },
+        {
+            from_port = 80
+            to_port = 80
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "opening 80"
+        },
+        {
+            from_port = 8080
+            to_port = 8080
+            protocol = "tcp"
+            cidr_blocks = ["0.0.0.0/0"]
+            description = "opening 8080"
+        }
+    ]
+}
